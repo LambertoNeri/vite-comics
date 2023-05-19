9ee29347096190b08d68da91705dbfe7b8c9a51e
LambertoNeri <neri.lamberto91@gmail.com>
2023-05-19 22:44:08 +0200

    add underline on navbar

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -11,20 +11,26 @@
 <template>
 <div class="submenu">
     {{ navObject }}
+    <div class="underline"></div>
+    
 </div>
 </template>
 
 
 
 <style lang="scss" scoped>
-    div{
+    .submenu{
         position: relative;
             &:hover{ 
             cursor: pointer;
             color: #0282F9;
+            
+            }
+            &:hover .underline{
+                display: block;
             }
 
-            &:after{
+            &::after{
             background-color: #0282F9;
             height: 2px;
             width: 100%;
@@ -33,5 +39,17 @@
             left: 0;
             right: 0;
             }  
+        }
+        .underline{
+            background-color: #0282F9;
+            height: 5px;
+            width: 106%;
+            position: absolute;
+            top: 3.9rem;
+            left: -3px;
+            right: 0;
+            display: none;
+            
+            
         }
 </style>
